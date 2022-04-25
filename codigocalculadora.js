750e2c1a41b1561e334a858595654160ec63dd5a
LanaCP <svetlanacacerespedraza2122@alumnos.fesac.es>
2022-04-25 13:00:44 +0200

    comienzo del programa

---
+++ codigocalculadora.js
@@ -0,0 +1,28 @@
+<script >
+
+    function sumar() {
+        
+    }
+
+    function restar() {
+        
+    }
+
+    function multiplicar() {
+        
+    }
+
+    function dividir() {
+        
+    }
+
+    function igual() {
+        
+    }
+
+    function corregir() {
+        
+    }
+
+    
+</script>
